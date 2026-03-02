5da2b1792038aeeb6e66bea8ce1f0183b39e2e25
DEXX001 <dexx00111@gmail.com>
2026-03-02 10:51:17 +0100

    ajout exo my_xor.c

---
+++ week-2/XOR/my_xor.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int ac, char **av)
+{
+    if (ac != 4)
+    {
+        perror("ERROR - Argument");
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *src = fopen(av[1], "rb");
+    FILE *dest = fopen(av[2], "wb");
+
+    if (src == NULL || dest == NULL)
+    {
+        perror("ERROR SUR LES FILES");
+        exit(EXIT_FAILURE);
+    }
+
+    unsigned char key = atoi(av[3]);
+
+    unsigned char buffer[4096];
+    size_t nb_lus;
+
+    while ((nb_lus = fread(buffer, 1, sizeof(buffer), src)) > 0)
+    {
+        for (int i = 0; i < nb_lus; i++)
+        {
+            buffer[i] = buffer[i] ^ key;
+        }
+
+        fwrite(buffer, 1, nb_lus, dest);
+        
+    }
+
+    fclose(src);
+    fclose(dest);
+
+    return 0;
+    
+}
